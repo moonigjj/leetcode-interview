e788ae2aceb06161d5ecaa8a4409dfaeee2e75bc
tangyue <tangyue@gosotech.com>
2019-12-19 16:17:50 +0800

    新的提交

---
+++ src/leetcode/easy/StringCompression.java
@@ -0,0 +1,44 @@
+/**
+ * chenxitech.cn Inc. Copyright (c) 2017-2019 All Rights Reserved.
+ */
+package leetcode.easy;
+
+/**
+ *443. String Compression
+ * Given an array of characters, compress it in-place.
+ *
+ * The length after compression must always be smaller than or equal to the original array.
+ *
+ * Every element of the array should be a character (not int) of length 1.
+ *
+ * After you are done modifying the input array in-place, return the new length of the array.
+ *
+ *
+ * Follow up:
+ * Could you solve it using only O(1) extra space?
+ *
+ *
+ * Example 1:
+ *
+ * Input:
+ * ["a","a","b","b","c","c","c"]
+ *
+ * Output:
+ * Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+ *
+ * Explanation:
+ * "aa" is replaced by "a2". "bb" is replaced by "b2". "ccc" is replaced by "c3".
+ * @author tangyue
+ * @version $Id: StringCompression.java, v 0.1 2019-11-27 11:08 tangyue Exp $$
+ */
+public class StringCompression {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int compress(char[] chars) {
+
+        return 0;
+    }
+}
